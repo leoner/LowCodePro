1c36f67375afffa6c93df74422356742f934145a
hui.kang <hui.kang@antgroup.com>
2022-06-25 14:16:21 +0800

    init

---
+++ src/pages/Home/index.tsx
@@ -0,0 +1,14 @@
+import Guide from '@/components/Guide';
+import { PageContainer } from '@ant-design/pro-components';
+import styles from './index.less';
+
+const HomePage: React.FC = () => {
+  return (
+    <PageContainer ghost>
+      <div className={styles.container}>
+      </div>
+    </PageContainer>
+  );
+};
+
+export default HomePage;
